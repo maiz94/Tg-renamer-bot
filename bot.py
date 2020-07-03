c233f1d47bafd38f91a171911d3998fe4b72fb8f
Maiz <7908304+maiz94@users.noreply.github.com>
2020-07-03 15:58:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,11 +28,11 @@ if __name__ == "__main__" :
         root="plugins"
     )
     app = pyrogram.Client(
-        "AnyDLBot",
+        "TGRenamerBot",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(683538773)
+    Config.AUTH_USERS.add(36420076)
     app.run()
